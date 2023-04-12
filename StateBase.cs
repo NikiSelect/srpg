c1e78630d54b95fd57dc106ff966f3996b51a6ac
やりこみ部屋nikiの <select.niki@gmail.com>
2023-04-12 11:34:55 +0000

    Upload New File

---
+++ StateBase.cs
@@ -0,0 +1,14 @@
+﻿
+public class StateBase<T> where T : StateMachineBase<T>
+{
+    protected T machine;
+    public StateBase(T _machine) { machine = _machine; }
+    public virtual void OnEnterState() { }
+    public virtual void OnUpdate() { }
+    public virtual void OnExitState() { }
+
+    public void ChangeState(StateBase<T> _nextState)
+    {
+        machine.ChangeState(_nextState);
+    }
+}
