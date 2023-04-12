0bacc70eddc9c2a1f7322aa151f5fcaef5e09ca3
やりこみ部屋nikiの <select.niki@gmail.com>
2023-04-12 11:29:28 +0000

    Upload New File

---
+++ BattleItemListWindowUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BattleItemListWindowUI : MonoBehaviour
+{
+    //スクリプト連携
+    [SerializeField] private BattleItemBanner battleItemBanner;
+    [SerializeField] private ItemManager itemManager;
+    [SerializeField] private GameManager gameManager;
+
+    //アイテムボタン
+    [SerializeField] private Transform rootItemList;
+    [SerializeField] private GameObject itemSlotPrefab;
+
+    //アニメーション
+    private Animator myAnimator;
+    public Animator MyAnimator => myAnimator ? myAnimator : (myAnimator = GetComponent<Animator>());
+
+    //アイテムスロット生成
+    public void setItemSlot(Character character)
+    {
+        MyAnimator.SetBool("isShow", true);
+        clearItemSlots();
+        foreach (var item in character.Items)
+        {
+            BattleItemBanner banner = Instantiate(itemSlotPrefab, rootItemList).GetComponent<BattleItemBanner>();
+            banner.setItemParameter(character, item, gameManager);
+        }
+    }
+
+    //アイテムスロットの削除
+    public void clearItemSlots()
+    {
+        foreach (Transform itemSlot in rootItemList)
+        {
+            Destroy(itemSlot.gameObject);
+        }
+    }
+}
