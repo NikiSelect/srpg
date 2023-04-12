cc8c78da1890e8e28c30a38ca09f349d387ebd9a
やりこみ部屋nikiの <select.niki@gmail.com>
2023-04-12 11:21:25 +0000

    Delete WindowUICommand.cs

--- WindowUICommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-public class WindowUICommand : MonoBehaviour
-{
-    //アニメーション
-    private Animator myAnimator;
-    public Animator MyAnimator => myAnimator ? myAnimator : (myAnimator = GetComponent<Animator>());
-}
