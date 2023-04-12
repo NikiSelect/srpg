c539451c8a3af369f6ce5cbfffc9195498e77b74
やりこみ部屋nikiの <select.niki@gmail.com>
2023-04-12 11:32:10 +0000

    Upload New File

---
+++ FadeOut.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+using DG.Tweening;
+
+public class FadeOut : MonoBehaviour
+{
+    //フェード用画像
+    [SerializeField] public Image fedeImage;
+
+    void Start()
+    {
+        Color color = gameObject.GetComponent<Image>().color;
+        color.a = 1f;
+        gameObject.GetComponent<Image>().color = color;
+        sceneFadeOutImage(() => { });
+    }
+
+    //フェードアウト
+    public void sceneFadeOutImage(Action onFadeEnd)
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(fedeImage.DOFade(0.0f, 1.0f).SetEase(Ease.InCubic).OnComplete(() =>
+        {
+            onFadeEnd.Invoke();
+        }));
+    }
+}
