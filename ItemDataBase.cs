b82f8003b8e36d7fa053db4b6eddadbd3602364f
やりこみ部屋nikiの <select.niki@gmail.com>
2023-04-12 11:33:07 +0000

    Upload New File

---
+++ ItemDataBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDataBase", menuName = "CreateItemDataBase")]
+public class ItemDataBase : ScriptableObject
+{
+    [SerializeField] private List<Item> itemLists = new List<Item>();
+
+    public List<Item> GetItemLists()
+    {
+        return itemLists;
+    }
+
+    public Item GetItemData(string itemName)
+    {
+        foreach (var item in itemLists)
+        {
+            if (item.GetItemName() == itemName)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+}
