f5ca1b9794538c50ea2c59450a6cfbd3754d9133
やりこみ部屋nikiの <select.niki@gmail.com>
2023-04-12 11:31:58 +0000

    Upload New File

---
+++ FadeIn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+using DG.Tweening;
+
+public class FadeIn : MonoBehaviour
+{
+    //フェード用画像
+    [SerializeField] public Image fedeImage;
+
+    void Start()
+    {
+        sceneFadeIn(() => { });
+    }
+
+    //フェードイン
+    public void sceneFadeIn(Action onFadeEnd)
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(fedeImage.DOFade(1.0f, 1.0f).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            onFadeEnd.Invoke();
+        }));
+    }
+}
