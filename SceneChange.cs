6e0c8ce0e7cccbfd5b37f685430c42411ba4761f
やりこみ部屋nikiの <select.niki@gmail.com>
2023-04-12 11:34:44 +0000

    Upload New File

---
+++ SceneChange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void TitleSceneChange()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void GameMainSceneChange()
+    {
+        SceneManager.LoadScene("GameMainScene");
+    }
+
+    public void GameClearSceneChange()
+    {
+        SceneManager.LoadScene("GameClearScene");
+    }
+    public void GameOverSceneChange()
+    {
+        SceneManager.LoadScene("GameOverScene");
+    }
+}
