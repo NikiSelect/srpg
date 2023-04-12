3f6ef16eefdae2af6ff826ae747344848dbdff44
やりこみ部屋nikiの <select.niki@gmail.com>
2023-04-12 11:35:08 +0000

    Upload New File

---
+++ StateMachineBase.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+public class StateMachineBase<T> : MonoBehaviour where T : StateMachineBase<T>
+{
+    private StateBase<T> m_currentState;
+    private StateBase<T> m_nextState;
+    public string stateName;
+
+    public bool ChangeState(StateBase<T> _nextState)
+    {
+        bool bRet = m_nextState == null;
+        if (m_currentState != null)
+        {
+            m_currentState.OnExitState();
+        }
+        m_currentState = _nextState;
+        m_currentState.OnEnterState();
+        m_nextState = null;
+        stateName = m_currentState.ToString();
+        return bRet;
+    }
+
+    protected virtual void Update()
+    {
+
+        if (m_currentState != null)
+        {
+            m_currentState.OnUpdate();
+        }
+    }
+}
